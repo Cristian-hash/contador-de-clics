5290fae5ca338a46fa914c0f26efb2c31934b607
Cristian-hash <cpacori@unsa.edu.pe>
2024-04-04 11:27:45 -0500

    2:58:19 Logo freeCodeCamp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <div className="freecodecamp-logo-contenendor" >
+      <img 
+      className="freecodecamp-logo" 
+      src={} 
+      alt="Logo de freeCodeCamp" />
+    </div>
+  </div>;
 }
 
 export default App;
