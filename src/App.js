3f714214ea66f3bc494a19c2d0c37225990805c7
Cristian-hash <cpacori@unsa.edu.pe>
2024-04-11 06:54:55 -0500

    3:00:06 Asignar estilos con CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
+import freeCodeCampLogo from "./imagenesfreecodecamp-logo.png";
 
 function App() {
-  return <div className="App">
-    <div className="freecodecamp-logo-contenendor" >
-      <img 
-      className="freecodecamp-logo" 
-      src={} 
-      alt="Logo de freeCodeCamp" />
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenendor">
+        <img
+          className="freecodecamp-logo"
+          src={freeCodeCampLogo}
+          alt="Logo de freeCodeCamp"
+        />
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
