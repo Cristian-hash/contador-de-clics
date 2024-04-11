cda91f55663fa654d222592f4345152e5ebdaa15
Cristian-hash <cpacori@unsa.edu.pe>
2024-04-11 11:02:08 -0500

    3:13:55 manejar click

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-function Boton({ texto, esBotonDeClic }) {
-  return <button className={esBotonDeClic ? "boton-clic":"boton-reiniciar"}}>{texto}</button>;
+function Boton({ texto, esBotonDeClic, manejarClic }) {
+  return (
+    <button
+      className={esBotonDeClic ? "boton-clic" : "boton-reiniciar"}
+      onClick={manejarClic}
+    >
+      {texto}
+    </button>
+  );
 }
+
+export default Boton;
