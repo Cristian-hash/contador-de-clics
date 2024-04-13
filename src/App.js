b7972ff39ce0bc0069dedd0bd5b68ff6529f954f
Cristian-hash <cpacori@unsa.edu.pe>
2024-04-13 09:17:28 -0500

    3:17:40 Definir Funcion en un Componente

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Boton from "./componentes/Boton";
 import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
 
 function App() {
@@ -12,6 +13,16 @@ function App() {
         />
       </div>
       <div className="contenedor-principal">
+        <Boton 
+          texto ="Clic"
+          esBotonDeClic={true} 
+          manejarClic={}/>
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false} 
+          manejarClic={}
+          />
+      </div>
     </div>
   );
 }
